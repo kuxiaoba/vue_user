8c69a0f40d966b460994e67b211f4f5863a649c3
kuxiaoba <1697928021@qq.com>
2019-07-14 19:41:08 +0800

    完成报表的开发

--- src/router.js
+++ src/router.js
@@ -11,6 +11,7 @@ import Params from './components/goods/Params.vue'
 import GoodList from './components/goods/List.vue'
 import Add from './components/goods/Add.vue'
 import Order from './components/order/Order.vue'
+import Report from './components/report/Report.vue'
 // 导入全局样式
 import './assets/css/global.css'
 
@@ -50,6 +51,9 @@ const router = new Router({
             }, {
                 path: '/orders',
                 component: Order
+            }, {
+                path: '/reports',
+                component: Report
             }]
         }
     ]
